0c8ba952c50e5b84f803822ca7afa3255508681c
MishaKonsta <molosnoww@ya.ru>
2021-11-28 18:29:28 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?PHP 
 $config = array (
 
-'home_title' => "Game Server Template",
+'home_title' => "Game Server Website",
 
-'home_logo_name' => "Server Name",
+'home_logo_name' => "Your Server Name",
 
-'home_short_description' => "Welcome to our server",
+'home_short_description' => "Welcome to our server!",
 
-'home_description' => "Welcome to ous server website, you can connect to our servers and view server stats & players",
+'home_description' => "Welcome to our gaming server website! You can connect to our server, or check server online & players, view online statistic",
 
 'our_server_button' => "Connect to server",
 
@@ -17,11 +17,22 @@ $config = array (
 'link2' => "https://google.com",
 'link2_name' => "Link 2",
 
-'home_about_server_description' => "This is full server description, you can edit it in config.php file.<br /><br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br /><br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+'home_about_server_description' => "
 
-'server_id' => "278", // Your Game Server ID on Play-Servers.com
+		<p>This is full <b>gaming server description</b>! You can edit it in config.php file, and you can use HTML code here.</p>
+		<br />
+		 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		 <br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		 <br /><br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 
+ ",
+ 
+ 
+ 
 
-// If you don't have Server ID, add your server on Play-Servers.com/add-server
+'server_id' => "41", // Your Game Server ID on Play-Servers.com (Check it on User Panel > Your Servers), or get this id from your server link. Example https://play-servers.com/server-11111 (11111 - Server ID)
+
+// If you don't have Server ID: Add your server on Play-Servers.com/add-server
 
 // Supported Games list - Play-Servers.com/#supportedgames
 
